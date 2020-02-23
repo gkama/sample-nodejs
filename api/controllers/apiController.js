dedc1969bf436c9cffefa00c9f4a7382638ae513
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-22 21:44:24 -0500

    update to add combinations method

--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -1,7 +1,24 @@
 "use strict";
 
-const testModel = require("../models/testModel")
+const testModel = require("../models/testModel");
 
 exports.getModels = function(req, res) {
-    res.json(testModel.Models)
+    res.json(testModel.Models);
+}
+
+exports.getCombinations = function(str) {
+    var fn = function(active, rest, a) {
+        if (!active && !rest) {
+            return;
+        }
+        if (!rest) {
+            a.push(active);
+        }
+        else {
+            fn(active + rest[0], rest.slice(1), a);
+            fn(active, rest.slice(1), a);
+        }
+        return a;
+    }
+    return fn("", str, []);
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var routes = require("./api/routes/apiRoutes"); //importing route
+const routes = require("./api/routes/apiRoutes"); //importing route
 
 const PORT = 8080;
 const HOST = "localhost";
